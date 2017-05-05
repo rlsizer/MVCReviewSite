6a884dddfb006b874da5e6f4e23e410f04c08f8a
rlsizer <theryanlilo@gmail.com>
2017-05-05 11:44:17 -0400

    Added Review class to models.

--- MVCRSProject/Models/Review.cs
+++ MVCRSProject/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,22 @@ namespace MVCRSProject.Models
 {
     public class Review
     {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Rating { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+
+        [Display(Name = "Date Published")]
+        [DisplayFormat(DataFormatString = "{0:M/d/yyyy}")]
+        public DateTime PublishedDate { get; set; }
+
+        
+        
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
