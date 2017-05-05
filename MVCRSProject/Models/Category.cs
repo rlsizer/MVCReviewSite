0d32211c9066aca20651016f5df28caa0818fa56
rlsizer <theryanlilo@gmail.com>
2017-05-05 13:16:06 -0400

    Added Category class to models.

--- MVCRSProject/Models/Category.cs
+++ MVCRSProject/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,14 @@ namespace MVCRSProject.Models
 {
     public class Category
     {
+
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+
+
     }
 }
