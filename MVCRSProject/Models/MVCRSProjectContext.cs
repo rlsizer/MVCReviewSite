4faadac26f4d8c27d889123ea305622eae61814a
rlsizer <theryanlilo@gmail.com>
2017-05-05 14:06:51 -0400

    Added ReviewsController and CategoriesController.

---
+++ MVCRSProject/Models/MVCRSProjectContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCRSProject.Models
+{
+    public class MVCRSProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MVCRSProjectContext() : base("name=MVCRSProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MVCRSProject.Models.Review> Reviews { get; set; }
+
+        public System.Data.Entity.DbSet<MVCRSProject.Models.Category> Categories { get; set; }
+    }
+}
